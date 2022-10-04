1acf7583863dd34cb0c5fde436d0bc36203feed8
ludovicdevio <ludovicdev.io@gmail.com>
2022-10-04 19:10:02 +0100

    Add proper title

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,9 @@
 @extends('base')
 
+@section('title')
+    About Us | Hello City
+@endsection
+
 @section('content')
     <p>Built with &hearts; by Ludovic Dev.</p>
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,10 @@
 @extends('base')
-<h1> Hello from Quebec! </h1>
+
+@section('title')
+    Hello City
+@endsection
 
 @section('content')
+    <h1> Hello from Quebec! </h1>
     <p> It's currently {{ date('h:i A') }}. </p>
 @endsection
